2b3d862dfd1d32e14952df1dab8a26dfc8749d04
QuinnJMosher <Darkstaro59@gmail.com>
2014-11-04 15:27:01 -0800

    header

---
+++ mathLib/mathLib/header/Vector2.h
@@ -0,0 +1,37 @@
+#ifndef _Vector2_h_
+#define _Vector2_h_
+
+class Vector2 {
+
+public:
+	float x;
+	float y;
+
+	Vector2();//x = 0 y = 0
+	Vector2(float in_x, float in_y);
+	Vector2(Vector2 in_copy);
+
+	float DotProduct(Vector2 other);
+	static float DotProduct(Vector2 in_a, Vector2 in_b);
+
+	float Magnitude();
+	static float Magnitude(Vector2 input);
+
+	Vector2 Normalize();
+	static Vector2 Normalize(Vector2 input);
+
+	void NormalizeThis();//edits the vector2 called upon instead of outputing
+
+	Vector2 operator+(Vector2 other);
+	Vector2 operator-(Vector2 other);
+	Vector2 operator*(Vector2 other);
+	Vector2 operator*(float input);
+
+	Vector2 operator+=(Vector2 other);
+	Vector2 operator-=(Vector2 other);
+	Vector2 operator*=(Vector2 other);
+	Vector2 operator*=(float input);
+
+};
+
+#endif
