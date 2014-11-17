3b885802526fa3cf5220ede59fd6064e3046cd76
QuinnJMosher <Darkstaro59@gmail.com>
2014-11-17 15:20:00 -0800

    lerp

--- mathLib/mathLib/sorce/Test.cpp
+++ mathLib/mathLib/sorce/Test.cpp
@@ -1,9 +1,13 @@
-#include "General.h"
+#include "Vector2.h"
 #include <iostream>
 
 using namespace std;
 
 void main() {
-	cout << General::ShiftPowOfTwo(6) << endl;
+	Vector2 vec1 = Vector2(20, 20);
+	Vector2 vec2 = Vector2(10, 10);
+	Vector2 vec3 = Vector2::Lerp(vec1, vec2, .5);
+
+	cout << vec3<< endl;
 	system("pause");
 }
