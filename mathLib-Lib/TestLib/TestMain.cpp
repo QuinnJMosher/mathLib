82a6574a8509238b64c71d01403ac71b46512334
QuinnJMosher <Darkstaro59@gmail.com>
2014-12-02 09:20:53 -0800

    rehooking gtest

--- mathLib-Lib/TestLib/TestMain.cpp
+++ mathLib-Lib/TestLib/TestMain.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "gtest\gtest.h";
+#include "QuinnMathLibAll.h"
 
-using namespace 
+using namespace std;
+
+TEST(sample_test_case, sample_test)
+{
+	EXPECT_EQ(1, 1);
+}
 
 int main(int argc, char** argv)
 {
-	
-	system("pause");
+
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	std::getchar(); // keep console window open until Return keystroke
 }
